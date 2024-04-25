fb80a32be292bb8173b5c4ed892f9307398db2d8
dzakii8 <dzakii8@gmail.com>
2024-04-25 13:09:45 +0700

    create frontend

--- backend/helpers_function/jwt.js
+++ backend/helpers_function/jwt.js
@@ -1,6 +1,6 @@
 var jwt = require('jsonwebtoken');
 
-function generateToken(payload) {return jwt.sign(payload, process.env.JWT_SECRET)}
-function verifyToken(token) {return jwt.verify(token, process.env.JWT_SECRET)}
+function generateToken(payload) {return jwt.sign(payload, process.env.JWT_SECRET ?? 'rahasia')}
+function verifyToken(token) {return jwt.verify(token, process.env.JWT_SECRET ?? 'rahasia')}
 
 module.exports = {generateToken, verifyToken}
